004ea7d08b1ebcf3db38ac767628227d937f7c11
Mortiz018 <mortiz018@citymail.cuny.edu>
2025-05-04 22:41:10 -0400

    Data Visualizations

--- main_app/routes.py
+++ main_app/routes.py
@@ -3,6 +3,24 @@ from flask import Blueprint, render_template, request, current_app, jsonify
 from main_app.utils.search import search_restaurants
 import logging
 
+# Data analysis and visualization imports
+from main_app.plots import total_inspections
+from main_app.plots import critical_violations
+from main_app.plots import average_score
+from main_app.plots import worst_borough
+from main_app.plots import create_grade_pie_chart
+from main_app.plots import create_grade_bar_chart
+from main_app.plots import create_grade_boro_bar_chart
+from main_app.plots import create_average_score_boro
+from main_app.plots import create_cuisines_chart
+from main_app.plots import create_cuisines_percentage_chart
+from main_app.plots import create_violations_per_cuisine_chart
+from main_app.plots import create_latest_violations_per_cuisine_chart
+from main_app.plots import create_avg_violations_by_cuisine_and_borough_chart
+from main_app.plots import create_violation_code_treemap
+
+
+
 bp = Blueprint('main', __name__)
 
 @bp.route('/')
@@ -35,3 +53,39 @@ def search():
     except Exception as e:
         logging.error(f"Search error: {str(e)}")
         return jsonify({'error': 'Search failed'}), 500
+    
+
+# data visualization route
+@bp.route('/graphs')
+def graphs():
+    """Render the graphs page."""
+    grade_pie = create_grade_pie_chart()
+    grade_bar = create_grade_bar_chart()
+    boro_bar = create_grade_boro_bar_chart()
+    cuisines_bar = create_cuisines_chart()
+    cuisines_percentage = create_cuisines_percentage_chart()
+    violations_per_cuisine = create_violations_per_cuisine_chart()
+    latest_violations_per_cuisine = create_latest_violations_per_cuisine_chart()
+    avg_violations_by_cuisine_and_borough = create_avg_violations_by_cuisine_and_borough_chart()
+    avg_score_boro = create_average_score_boro()
+    violation_code = create_violation_code_treemap()
+    all_inspections = "{:,}".format(total_inspections())
+    critical = "{:,}".format(critical_violations())
+    avg_score = average_score()
+    bad_borough = worst_borough()
+    return render_template('graphs.html',
+                           all_inspections=all_inspections,
+                           critical=critical,
+                           avg_score=avg_score,
+                           bad_borough=bad_borough, 
+                           grade_pie=grade_pie, 
+                           grade_bar=grade_bar, 
+                           boro_bar=boro_bar,
+                           avg_score_boro=avg_score_boro,
+                           cuisines_bar=cuisines_bar,
+                           cuisines_percentage=cuisines_percentage,
+                           violations_per_cuisine=violations_per_cuisine,
+                           latest_violations_per_cuisine = latest_violations_per_cuisine,
+                           avg_violations_by_cuisine_and_borough=avg_violations_by_cuisine_and_borough,
+                           violation_code=violation_code
+                           )
